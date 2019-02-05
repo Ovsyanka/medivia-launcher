8a950e83565155a21d2e1aafeb03c5bdeb26983b
or <igor_deyawka@mail.ru>
2019-02-05 14:09:09 +0200

    Initial commit, pre-alpha

---
+++ README.md
@@ -0,0 +1,44 @@
+# About
+
+It is the launcher for the Medivia game.
+
+Disclaimer: By now it is just proof of concept to see if it is in demand at all. So the installation process isn't automated well, documentation is poor and etc. You always can reach me on the Medivia Discord server (name: Ovsyanka) if you have questions or suggestions. If you interested in this tool and like to motivate me to improve it - please contact with me.
+
+## Functionality
+
+The main functionality it provides:
+
+* You can have distinct "profiles" of settings (called instancies)
+* On each closing of the client launcher saves the game configs and store all the history of changes.
+
+# Installation
+
+
+You have to install:
+
+* [Python 3.7](https://www.python.org/downloads/)
+* [click](https://pypi.org/project/click/)
+* [pygit2](https://www.pygit2.org/install.html)
+
+Then download the source code from the github (the green download button) and extract it somewhere you like.
+
+# Usage
+
+```
+python medivia-launcher.py --executable=<path to the medivia binary> --instance=<instanceName>
+```
+
+# Under the hood
+
+When you run the launcher at the first time it creates it's own configuration folder in `$HOME/.medivia-launcher`. It will not make any changes to your medivia client configuration folder `$HOME/.medivia`. Then it copies all content from the medivia client configuration folder `$HOME/.medivia` to the `$HOME/.medivia-launcher/originConfig`.
+
+Each instance has it's own config folder `$HOME/.medivia-launcher/instancies/<instanceName>/`.
+
+When you run the medivia client through the launcher it uses the instance config folder instead of origin client config folder.
+
+TODO: finish the explanation ...
+
+# Known problems
+
+* The config file content have no expliced order. This leads to the big differences in config on each save
+* The map file is binary and because of that the size of the repository will grow significantly
---
+++ main.py
@@ -0,0 +1,128 @@
+import os
+import shutil
+import subprocess
+from distutils.dir_util import copy_tree
+
+import click
+from pygit2 import Repository
+
+import gitOperations
+
+homePath = os.environ['HOME']
+
+# settings
+gameConfigPath = os.path.join(homePath, '.medivia')
+configsRootPath = os.path.join(homePath, '.medivia-launcher')
+# override for development
+# configsRootPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.medivia-launcher')
+originConfigPath = os.path.join(configsRootPath, 'originConfig')
+instanciesConfigPath = os.path.join(configsRootPath, 'instancies')
+# configRepoSkeletonPath =
+
+# TODO: make the command to print it
+def version():
+    print("0.0.1")
+
+@click.command()
+@click.option('--instance', default='instance1', help='Instance name.')
+@click.option('--executable', default='medivia', help='Full path for medivia executable ("medivia" by default).')
+# @click.argument('executable')
+def launchMedivia(executable='medivia', instance=None):
+    print(executable)
+    print(instance)
+
+    # if var1 is None:
+    # var1 = 4
+    # instance = 'test'
+    currentHomePath = os.path.join(instanciesConfigPath, instance)
+    pidFilePath = os.path.join(currentHomePath, 'pid')
+
+    # ensure origin configs exists
+    if not os.path.isdir(originConfigPath):
+        createOrigin(gameConfigPath, originConfigPath)
+
+    # ensure instance path exists
+    if not os.path.isdir(currentHomePath):
+        # os.mkdir(currentHomePath)
+        createInstance(path=currentHomePath, originPath=originConfigPath)
+
+    # TODO: create commit if any changes in the instance after the last commit
+
+    # TODO: return error
+    if os.path.isfile(pidFilePath):
+        exit
+
+    env = os.environ.copy()
+    env["HOME"] = currentHomePath
+    process = subprocess.Popen([executable], env=env)
+
+    pid = process.pid
+
+    pidFile = open(pidFilePath, "w")
+    pidFile.write(str(pid))
+    pidFile.close()
+
+    process.communicate()
+
+    os.remove(pidFilePath)
+
+    print('commiting changes')
+
+    # commiting any changes made
+    instanceRepo = gitOperations.getRepo(currentHomePath)
+    gitOperations.commitAll(instanceRepo)
+
+    print('end')
+
+# def onStart():
+#     pass
+
+
+def createInstance(path: str, originPath: str) -> None:
+    """
+    Creates new instance. Path should't be existed.
+
+    originPath should be the path to the origin repository. It will be cloned to the path and saved as `origin` remote repo.
+    The current commit will be `master` and the new local branch will be created: `instance/<name>`
+
+    TODO: replace the `path` to the `name` or add the `name` parameter
+    TODO: implement and return the Instance class?
+
+    Arguments:
+        path {str} -- instance path
+        originPath {str} -- path to the origin repo
+    """
+
+    # можно перехватывать FileExistsError и выводить норм сообщение
+    # if os.path.isdir(currentHomePath):
+    # os.mkdir(path)
+
+    instanceName = os.path.basename(os.path.normpath(path))
+
+    # TODO: test if remote added
+    print("cloning from " + originPath + " to " + path)
+    repo = gitOperations.cloneOrigin(originPath, path)
+    gitOperations.makeInstanceBranch(repo, instanceName)
+
+
+def createOrigin(gameConfigsPath, targetPath):
+    # copying all from configs from original game configs folder
+    # shutil.copytree(gameConfigsPath, targetPath)
+    # targetPath = os.path.relpath(targetPath)
+    print("creating " + targetPath)
+    os.makedirs(targetPath)
+    
+    copy_tree(gameConfigsPath, os.path.join(targetPath, '.medivia'))
+
+    gitignoreFile = open(os.path.join(targetPath, ".gitignore"), "w")
+    gitignoreFile.write(".nv\n")
+    gitignoreFile.write("pid\n")
+    gitignoreFile.close()
+
+    repo = gitOperations.initRepository(targetPath)
+    gitOperations.commitAll(repo, 'initial copy of the settings directory')
+
+
+if __name__ == '__main__':
+    launchMedivia()
+    # createOrigin(gameConfigPath, './tmpTest')
